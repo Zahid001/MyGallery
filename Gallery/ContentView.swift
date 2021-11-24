ee5a939dd548b90b5982b1ca5129a032642658e9
Zahidul000 <zahidul@royex.net>
2021-11-24 11:56:20 +0600

    project initialized

---
+++ Gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gallery
+//
+//  Created by Md Zahidul Islam Mazumder on 24/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Md Zahidul Islam Mazumder on 24/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
